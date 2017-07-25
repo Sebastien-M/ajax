85db2981ff71b23de0d941ed86cc1531f8b3a85c
Sébastien Mandaba <seb.mandaba@gmail.com>
2017-07-25 14:04:55 +0200

    Functions created

---
+++ js/ajax.js
@@ -0,0 +1,35 @@
+
+function doajax(options) {
+    let defaults = {
+        url: '',
+        method: 'GET',
+        async: true,
+        args: '',
+        callback: function () {},
+        callbackError: function () {}
+    }
+    assignArgs(options,defaults)
+
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+            if (ajax.status === 200 || ajax.status === 304) {
+                defaults.callback(ajax.response);
+            } else {
+                defaults.callbackError();
+            }
+        }
+    }
+
+    ajax.open(defaults.method, defaults.url, defaults.async);
+    ajax.send(defaults.args);
+
+}
+
+function assignArgs(source,target){
+    for(let clef in source){
+        if(target.hasOwnProperty(clef)){
+            target[clef] = source[clef];
+        }
+    }
+}
